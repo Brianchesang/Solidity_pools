d949e8f942e837ddaf5d6b339f20093377e9769b
"brian chesang" <"brianchesang@gmail.com">
2021-12-13 12:06:58 +0300

    commit

--- Fund Contract.sol
+++ Fund Contract.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
- pragma solidity ^0.6.0;
+ pragma solidity 0.6.0<=0.9.0;
 
  contract fund_contract{
+     struct entity {
+         address payable  Address;
+         uint256 contribution;
+     }
+
+    uint256 [] public  poolMembers;
+    uint256  public totalPoolValue ;  
+
+
+    
      
  }
 
