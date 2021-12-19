bc2890a92843630b454c8bb13d30591f072dd0f4
"brian chesang" <"brianchesang@gmail.com">
2021-12-19 09:20:41 +0300

    commit

--- Fund Contract.sol
+++ Fund Contract.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
  pragma solidity 0.6.0<=0.9.0;
+ import"@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
  contract fund_contract{
      struct entity {
@@ -8,7 +9,23 @@
      }
 
     uint256 [] public  poolMembers;
-    uint256  public totalPoolValue ;  
+    uint256  public totalPoolValue ; 
+
+    
+    function getEthPrice() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,)  = priceFeed.latestRoundData();
+        return uint256(answer) ;
+    }
+
+    function getEthUsdConversionRAte(uint256 ethAmount) public view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethAmount * ethPrice );
+        return ethAmountInUSD;
+    }
+
+
+
 
 
     
